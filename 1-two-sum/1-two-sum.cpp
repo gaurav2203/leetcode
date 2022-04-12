5d9714bf312261132e7d9aa1fe617bbab7a76757
Gaurav Tank <74139026+gaurav2203@users.noreply.github.com>
2022-04-12 18:55:36 +0530

    Time: 15 ms (73.10%), Space: 10.6 MB (55.65%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        for(int i=0; i< nums.size(); i++){
-            for(int j=i+1; j< nums.size(); j++){
-                if(nums[i] + nums[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                }
-            }
+        int n = nums.size();
+        vector<pair<int, int>> arr(n);
+        for (int i = 0; i < n; ++i) {
+            arr[i] = make_pair(nums[i], i);
         }
-        return ans;
+        sort(arr.begin(), arr.end()); 
+        int left = 0, right = n- 1;
+        while (left < right) {
+            int sum2 = arr[left].first + arr[right].first;
+            if (sum2 == target) 
+                return {arr[left].second, arr[right].second};
+            if (sum2 > target)
+                right -= 1; 
+            else
+                left += 1; 
+        }
+        return {};
     }
 };
+
+// Complexity
+
+// Time: O(N * logN), where N <= 10^4 is number of elements in the array nums.
+// Space: O(N)
