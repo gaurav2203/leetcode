7cd95c02e8b5b0b3cdfa536c2685d81104e2ab6d
Gaurav Tank <74139026+gaurav2203@users.noreply.github.com>
2022-06-28 22:03:53 +0530

    Time: 8 ms (17.35%), Space: 8.9 MB (89.53%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -3,24 +3,24 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int p1= m-1;
         int p2= n-1;
-        int rep_ptr= m+n- 1;
+        int ptr= m+n -1;
         
-        while(p2>=0 && p1 >=0 && p1<=rep_ptr){
-            if(nums1[p1] < nums2[p2]){
-                nums1[rep_ptr]= nums2[p2];
+        while(p1 >= 0 && p2 >= 0){
+            if(nums1[p1] <= nums2[p2]){
+                nums1[ptr] = nums2[p2];
                 p2--;
-                rep_ptr--;
+                ptr--;
             }
             else{
-                swap(nums1[p1], nums1[rep_ptr]);
+                nums1[ptr]= nums1[p1];
                 p1--;
-                rep_ptr--;
+                ptr--;
             }
         }
         while(p2 >=0){
-            nums1[rep_ptr]= nums2[p2];
-            rep_ptr--;
+            nums1[ptr]= nums2[p2];
             p2--;
+            ptr--;
         }
     }
 };
