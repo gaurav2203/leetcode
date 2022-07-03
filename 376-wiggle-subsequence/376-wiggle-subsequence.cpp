ee6049ce0261d44f6d271ee8112c0b588ab7da01
Gaurav Tank <74139026+gaurav2203@users.noreply.github.com>
2022-07-03 11:51:50 +0530

    Time: 4 ms (56.21%), Space: 7.1 MB (75.45%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -21,3 +21,42 @@ public:
         return max(up, down);
     }
 };
+
+/*
+int wiggleMaxLength(vector<int>& nums) {
+        int n= nums.size();
+        int ans= 0;
+        
+        for(int i=0; i< n- 1; i++){
+            nums[i]= nums[i+ 1]- nums[i];
+        }
+        int flag;
+        int i=0;
+        while(i< n){
+            if(nums[i]== 0){
+                i++;
+            }
+            else if(nums[i]> 0){
+                flag= true;
+                break;
+                
+            }
+            else{
+                flag= false;
+                break;
+            }
+        }
+        
+        for(int i=0; i< n- 1; i++){
+            if( nums[i]> 0 && flag== true){
+                ans++;
+                flag= false;
+            }
+            else if(nums[i]< 0 && flag== false){
+                ans++;
+                flag= true;
+            }
+        }
+        return ++ans;
+    }
+*/
