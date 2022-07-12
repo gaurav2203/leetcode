3f86f68634529d7d1d9a8ac573215012ab8da227
Gaurav Tank <74139026+gaurav2203@users.noreply.github.com>
2022-07-12 14:09:32 +0530

    Time: 10 ms (42.42%), Space: 7.4 MB (76.53%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,44 +1,17 @@
 class Solution {
-private:
-    char toLower(char ch){
-        if( (ch>= 'a' && ch <='z') || (ch>= '0' && ch <='9') )
-            return ch;
-        else{
-            char temp= ch -'A' +'a';
-            return temp;
-        }
-    }
-    bool isValid(char ch){
-        if( (ch>= 'a' && ch <='z') || (ch>= 'A' && ch <='Z') || (ch>= '0' && ch <='9') )
-            return 1;
-        return 0;
-    }
-    bool checkPalindrome(string s){
-        int start= 0;
-        int end= s.length()- 1;
-        
-        while(start <= end){
-            if(s[start] != s[end])
-                return 0;
-           else{
-               start++;
-               end--;
-           }
-        }
-        return 1;
-    }
 public:
     bool isPalindrome(string s) {
-        string temp= "";
+        int l= 0;
+        int r= s.length()- 1;
         
-        for(int j=0; j< s.length(); j++){
-            if(isValid(s[j]))
-                temp.push_back(s[j]);
+        while(l<r){
+            if(!isalnum(s[l])) l++;
+            else if(!isalnum(s[r])) r--;
+            else if(tolower(s[l]) != tolower(s[r])) return false;
+            else{
+                l++; r--;
+            }
         }
-        
-        for(int j=0; j< temp.length(); j++){
-            temp[j]= toLower(temp[j]);
-        }
-        return checkPalindrome(temp);
+        return true;
     }
 };
