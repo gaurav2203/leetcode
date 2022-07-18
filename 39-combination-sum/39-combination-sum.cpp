b7de4051d69073fbbd8c5a362a8124135e00d5bd
Gaurav Tank <74139026+gaurav2203@users.noreply.github.com>
2022-07-18 11:56:36 +0530

    Time: 14 ms (56.34%), Space: 11.1 MB (68.79%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,26 +1,20 @@
 class Solution {
-private:
-    void finalCombination(int start, int target, vector<int>& arr, vector<vector<int>>& _final, vector<int>& _temp){
-        if(start == arr.size()){
-            if(target == 0)
-                _final.push_back(_temp);
-            return;
-        }
-        
-        if(arr[start] <= target){
-            _temp.push_back(arr[start]);
-            finalCombination(start, target- arr[start], arr, _final, _temp);
-            _temp.pop_back();
-        }
-        finalCombination(start+ 1, target, arr, _final, _temp);
-        
-    }
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> _final;
-        vector<int> _temp;
-        
-        finalCombination(0, target, candidates, _final, _temp);
-        return _final;
+    void helper(vector<int>& nums, int target, vector<vector<int>>& ans, vector<int>& temp, int idx){
+        if(idx == nums.size()) return;
+        if(target == 0) return ans.push_back(temp);
+        if(target > 0){
+            temp.push_back(nums[idx]);
+            helper(nums, target- nums[idx], ans, temp, idx);
+            temp.pop_back();
+        }
+        helper(nums, target, ans, temp, idx+ 1);
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        sort(nums.begin(), nums.end());
+        helper(nums, target, ans, temp, 0);
+        return ans;
     }
 };
