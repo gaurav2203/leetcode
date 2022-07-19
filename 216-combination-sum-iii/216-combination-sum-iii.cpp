a6227a19d2cd3487227a048c4b787061ecf6a2f7
Gaurav Tank <74139026+gaurav2203@users.noreply.github.com>
2022-07-19 20:31:06 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (61.97%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    void helper(int k, int target, vector<int> mapper, vector<vector<int>>& ans, vector<int>& temp, int idx){
+    void helper(int k, int target, vector<vector<int>>& ans, vector<int>& temp, int idx){
         if(k == 0 && target == 0) return ans.push_back(temp);
         if(k == 0) return;
         
-        for(int i= idx; i< mapper.size(); i++){
-            if(mapper[i] > target) break;
-            temp.push_back(mapper[i]);
-            helper(k- 1, target- mapper[i], mapper, ans, temp, i+ 1);
+        for(int i= idx; i< 10; i++){
+            if(i > target) break;
+            temp.push_back(i);
+            helper(k- 1, target- i, ans, temp, i+ 1);
             temp.pop_back();
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> mapper= {1, 2, 3, 4, 5, 6, 7, 8, 9};
         vector<vector<int>> ans;
         vector<int> temp;
-        helper(k, n, mapper, ans, temp, 0);
+        helper(k, n, ans, temp, 1);
         return ans;
     }
 };
